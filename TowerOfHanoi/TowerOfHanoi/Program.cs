7855de60047dcd9c7623c719c0bb2b9802a0c280
lagunasj <Johnny.Lagunas@nov.com>
2021-07-21 13:42:26 -0500

    tower of hanoi solution

---
+++ TowerOfHanoi/TowerOfHanoi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi
+{
+    class Program
+    { 
+        public static double towerHanoi(int num)
+        {
+            return Math.Pow(2, num) - 1;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+                int number;
+                if (Int32.TryParse(input, out number))
+                {
+                    Console.WriteLine(String.Format("Number of steps: {0} \r\n",towerHanoi(number)));
+                }
+            }
+        }
+    }
+
+}
